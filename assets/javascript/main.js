dac1f7e76fd556e9e5ca45fcb08713b2adddecbe
Roman Verner <romverj@gmail.com>
2018-12-27 14:54:29 +0100

    Added peak info

---
+++ assets/javascript/main.js
@@ -0,0 +1,285 @@
+// Setting global variables
+var peaks = {
+    one: {
+        name: 'Mount Marcy',
+        height: 5344,
+        rank: 1,
+        coordinates: [44.112734, -73.923726],
+        url: "https://en.wikipedia.org/wiki/Mount_Marcy_(New_York)"
+    },
+    two: {
+        name: 'Algonquin Peak',
+        height: 5114,
+        rank: 2,
+        coordinates: [44.143611, -73.986667],
+        url: "https://en.wikipedia.org/wiki/Algonquin_Peak"
+    },
+    three: {
+        name: 'Mount Haystack',
+        height: 4960,
+        rank: 3,
+        coordinates: [44.105556, -73.900556],
+        url: "https://en.wikipedia.org/wiki/Mount_Haystack"
+    },
+    four: {
+        name: 'Mount Skylight',
+        height: 4920,
+        rank: 4,
+        coordinates: [44.099444, -73.930833],
+        url: "https://en.wikipedia.org/wiki/Mount_Skylight"
+    },
+    five: {
+        name: 'Whiteface Mountain',
+        height: 4867,
+        rank: 5,
+        coordinates: [44.365833, -73.902778],
+        url: "https://en.wikipedia.org/wiki/Whiteface_Mountain"
+    },
+    six: {
+        name: 'Dix Mountain',
+        height: 4857,
+        rank: 6,
+        coordinates: [44.082222, -73.786389],
+        url: "https://en.wikipedia.org/wiki/Dix_Mountain"
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    },
+    three: {
+        name: ,
+        height: ,
+        rank: ,
+        coordinates: []
+    }
+};
